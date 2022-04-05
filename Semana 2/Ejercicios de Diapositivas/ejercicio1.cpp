7ec34eae9b1b42fed406570e2a6d548ec54c5677
jariasf <jariasf03@gmail.com>
2022-04-05 02:40:36 -0500

    Fixed indentation of week2 files

--- Semana 2/Ejercicios de Diapositivas/ejercicio1.cpp
+++ Semana 2/Ejercicios de Diapositivas/ejercicio1.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-	// Declaración de variables
-	int numero, doble, triple;
+    // Declaración de variables
+    int numero, doble, triple;
 
-	// Entrada
-	cout << "Ingrese un numero entero: ";
-	cin >> numero;
+    // Entrada
+    cout << "Ingrese un numero entero: ";
+    cin >> numero;
 
-	// Proceso
-	doble = numero * 2;
-	triple = numero * 3;
+    // Proceso
+    doble = numero * 2;
+    triple = numero * 3;
 
-	// Salida
-	cout << "El valor es: " << numero << endl;
-	cout << "El doble es: " << doble << endl;
-	cout << "El triple es: " << triple << endl;
+    // Salida
+    cout << "El valor es: " << numero << endl;
+    cout << "El doble es: " << doble << endl;
+    cout << "El triple es: " << triple << endl;
 
 	return 0;
 }
